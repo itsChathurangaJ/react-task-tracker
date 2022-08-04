a9b41bdf7ed8c70e264efe281b9f8629bded2574
itsChathurangaJ <chathuranga.jayanidu@gmail.com>
2022-08-04 14:51:15 +0530

    added tasks and task components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import Header from "./components/Header";
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'task 1',
+      day: '1',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'task 2',
+      day: '2',
+      reminder: false,
+    },
+    {
+      id: 3,
+      text: 'task 3',
+      day: '3',
+      reminder: true,
+    },
+  ]);
+
   return (
-    <div className="container">
+    <div className='container'>
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
-}
+};
 
 export default App;
